e5420953e8ef977658f3a9b87cc7536e0a3bf09d
enternalwind <61649229+enternalwind@users.noreply.github.com>
2023-01-11 14:02:46 +0800

    242-Vaild-Anagram Runtime 48 ms Memory 15.2 MB
    First time no use leethub to push the leetcode solution and code

---
+++ 242-Valid -Anagram/242-Valid -Anagram.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!= len(t):
+            return False
+        else:
+            s_dic ={'a':0,'b':0,'c':0,'d':0,'e':0,'f':0,'g':0,'h':0,'i':0,'j':0,'k':0,
+            'l':0,'m':0,'n':0,'o':0,'p':0,'q':0,'r':0,'s':0,'t':0,'u':0,'v':0,'w':0,'x':0,'y':0,'z':0
+            }
+            t_dic ={'a':0,'b':0,'c':0,'d':0,'e':0,'f':0,'g':0,'h':0,'i':0,'j':0,'k':0,
+            'l':0,'m':0,'n':0,'o':0,'p':0,'q':0,'r':0,'s':0,'t':0,'u':0,'v':0,'w':0,'x':0,'y':0,'z':0
+            }
+            for i in range(len(s)):
+                if s[i] in s_dic:
+                    s_dic[s[i]]=s_dic[s[i]]+1
+                if t[i] in t_dic:
+                    t_dic[t[i]]=t_dic[t[i]]+1
+            if s_dic == t_dic:
+                return True
+            else:
+                return False
